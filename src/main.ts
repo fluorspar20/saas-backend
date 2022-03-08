fbfeeb7553f4f4e36ed729834b89585d7e102436
Akash Gupta <agbichpuriya@gmail.com>
2022-03-08 23:57:29 +0530

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('SaaS Docs')
+    .setDescription('The SaaS API description')
+    .setVersion('1.0')
+    .addTag('saas')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();

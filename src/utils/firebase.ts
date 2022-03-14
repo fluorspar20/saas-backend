738201f6f22125a4686d079833cc56f85e326cd5
Amogh Dixit <amoghdixit104@gmail.com>
2022-03-14 14:48:48 +0530

    chore: integrated server and firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,25 @@
+import * as admin from 'firebase-admin';
+import * as fireorm from 'fireorm';
+require('json-dotenv')();
+
+let firebaseApp: admin.app.App | null = null;
+export function getFirebaseApp() {
+  return firebaseApp;
+}
+
+export async function initFirebase() {
+  if (!firebaseApp) {
+    // console.log(process.env.project_id, process.env.client_email);
+
+    firebaseApp = admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.project_id,
+        privateKey: process.env.private_key,
+        clientEmail: process.env.client_email,
+      }),
+    });
+    const firestore = admin.firestore();
+    firestore.settings({ timestampsInSnapshots: true });
+    fireorm.initialize(firestore);
+  }
+}
